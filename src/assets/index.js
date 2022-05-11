a548e5d8ece1b57bf147a32f4b8f7778abe415ef
Jonathan Adami <pitilezard@gmail.com>
2022-05-11 22:24:57 +1000

    Start the grind until I make sense of anything

---
+++ src/assets/index.js
@@ -0,0 +1,19 @@
+import arches from "./arches";
+import stones from "./stones";
+import torii from "./torii";
+import hexagons from "./hexagons";
+import towers from "./towers";
+import trees from "./trees";
+import apparatus from "./apparatus";
+import mushrooms from "./mushrooms";
+
+export default {
+    arches,
+    stones,
+    torii,
+    hexagons,
+    towers,
+    trees,
+    apparatus,
+    mushrooms,
+};
---
+++ src/presets/index.js
@@ -0,0 +1,40 @@
+import defaultPreset from "./default";
+import contactPreset from "./contact";
+import egyptPreset from "./egypt";
+import checkerboardPreset from "./checkerboard";
+import forestPreset from "./forest";
+import goalandPreset from "./goaland";
+import yavapaiPreset from "./yavapai";
+import goldminePreset from "./goldmine";
+import threetowersPreset from "./threetowers";
+import poisonPreset from "./poison";
+import archesPreset from "./arches";
+import tronPreset from "./tron";
+import japanPreset from "./japan";
+import dreamPreset from "./dream";
+import volcanoPreset from "./volcano";
+import starryPreset from "./starry";
+import osirisPreset from "./osiris";
+import moonPreset from "./moon";
+
+export default {
+    none: {},
+    default: defaultPreset,
+    contact: contactPreset,
+    egypt: egyptPreset,
+    checkerboard: checkerboardPreset,
+    forest: forestPreset,
+    goaland: goalandPreset,
+    yavapai: yavapaiPreset,
+    goldmine: goldminePreset,
+    threetowers: threetowersPreset,
+    poison: poisonPreset,
+    arches: archesPreset,
+    tron: tronPreset,
+    japan: japanPreset,
+    dream: dreamPreset,
+    volcano: volcanoPreset,
+    starry: starryPreset,
+    osiris: osirisPreset,
+    moon: moonPreset,
+};
